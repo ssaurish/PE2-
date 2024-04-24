9ce563aee2ece52e6cc2e7e125f1a6156a9bb035
ssaurish <124463063+ssaurish@users.noreply.github.com>
2024-04-24 19:31:57 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,46 @@
+function getCookie(cookieName) {
+  var cookiesArray = document.cookie.split("; ");
+  for (var i = 0; i < cookiesArray.length; i++) {
+    var cookie = cookiesArray[i];
+    var cookieParts = cookie.split("=");
+    if (cookieParts[0] === cookieName) {
+      return cookieParts[1];
+    }
+  }
+  return null;
+}
+
+
+
+function handleLogin() {
+  document.getElementById("signInButton").disabled = true;
+  google.accounts.id.prompt((notification) => {
+    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+      console.log("SKIPPED :  " + notification.getSkippedReason());
+      console.log("NOT DISPLAYED :  " + notification.getNotDisplayedReason());
+      google.accounts.id.cancel();
+      google.accounts.id.prompt();
+      document.getElementById("signInButton").disabled = false;
+    }
+  });
+}
+
+function initializeGSI() {
+  google.accounts.id.initialize({
+    client_id:
+      "679398685666-kd43rioln3vcgbhr61pno8vpmgbfn8ru.apps.googleusercontent.com",
+    callback: handleGoogleSignIn,
+    response_type: "token",
+    use_fedcm_for_prompt : true,
+  });
+}
+
+function handleGoogleSignIn(googleUser) {
+  const tokenData = JSON.parse(
+    atob(
+      googleUser.credential.split(".")[1].replace(/-/g, "+").replace(/_/g, "/")
+    )
+  );
+  const email = tokenData.email;
+  alert(`Welcome ${tokenData.name}\nUsing Email : ${email}`)
+}
